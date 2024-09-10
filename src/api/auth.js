93fe99b9da364e730b84b28cc02e8ebfb7a15bb6
dev-rjw <rjw0105@gmail.com>
2024-09-10 10:30:39 +0900

    feat : jwt 인증 API 연결

---
+++ src/api/auth.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const API_URL = "https://moneyfulpublicpolicy.co.kr";
+
+export const register = async (userData) => {
+    const response = await axios.post(`${API_URL}/register`, userData);
+    return response.data;
+};
+
+export const login = async (userData) => {
+    const response = await axios.post(`${API_URL}/login`, userData);
+    return response;
+};
+
+export const getUserProfile = async (token) => {
+    const response = await axios.get(`${API_URL}/login`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    return response;
+};
+
+export const updateProfile = async (token, formData) => {
+    const response = await axios.fetch(`${API_URL}/profile`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        formData,
+    });
+    return response;
+};
