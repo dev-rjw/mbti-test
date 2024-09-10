8f85bd2e66a9b9c6c3761049c6768dfb443e8781
dev-rjw <rjw0105@gmail.com>
2024-09-10 20:29:11 +0900

    feat : context 사용 준비

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,50 @@ import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import TestPage from "./pages/TestPage";
 import TestResultPage from "./pages/TestResultPage";
+import { createContext, useState } from "react";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+export const UserContext = createContext();
 
 function App() {
+    const [user, setUser] = useState(null);
+
     return (
         <>
             <BrowserRouter>
-                <Layout>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/signup" element={<Signup />} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/test" element={<TestPage />} />
-                        <Route path="/results" element={<TestResultPage />} />
-                    </Routes>
-                </Layout>
+                <UserContext.Provider value={{ user, setUser }}>
+                    <Layout user={user} setUser={setUser}>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/login" element={<Login setUser={setUser} />} />
+                            <Route path="/signup" element={<Signup />} />
+                            <Route
+                                path="/profile"
+                                element={
+                                    <ProtectedRoute user={user}>
+                                        <Profile user={user} setUser={setUser} />
+                                    </ProtectedRoute>
+                                }
+                            />
+                            <Route
+                                path="/test"
+                                element={
+                                    <ProtectedRoute user={user}>
+                                        <TestPage user={user} />
+                                    </ProtectedRoute>
+                                }
+                            />
+                            <Route
+                                path="/result"
+                                element={
+                                    <ProtectedRoute user={user}>
+                                        <TestResultPage user={user} />
+                                    </ProtectedRoute>
+                                }
+                            />
+                        </Routes>
+                    </Layout>
+                </UserContext.Provider>
             </BrowserRouter>
         </>
     );
