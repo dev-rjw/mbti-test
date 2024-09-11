760dcbc53f52483019d169ae6d7984eda49003de
dev-rjw <rjw0105@gmail.com>
2024-09-11 15:41:09 +0900

    feat : 로그인 시 로그인하기 빼기

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
                 <UserContext.Provider value={{ user, setUser }}>
                     <Layout user={user} setUser={setUser}>
                         <Routes>
-                            <Route path="/" element={<Home />} />
+                            <Route path="/" element={<Home user={user} />} />
                             <Route path="/login" element={<Login setUser={setUser} />} />
                             <Route path="/signup" element={<Signup />} />
                             <Route
